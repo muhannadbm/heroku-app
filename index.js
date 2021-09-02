9b818812d60e3961dbfecf570d9ff32b735e7889
muhannadbm <mhd31145@hotmail.com>
2021-09-02 12:58:35 -0400

    deployable

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const server = express()
+
+
+const PORT = process.env.PORT || 4000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/', (req,res)=> {
+    res.send('<h1>I am awesome </h1>')
+})
+
+server.get('/api', (req,res)=>{
+    res.json({message: 'i Rock'})
+})
+
+server.listen(PORT, ()=>{
+    console.log('Listening on Port',PORT)
+})

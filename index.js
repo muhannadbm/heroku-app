fbd167fa277994a0cd8d482df7156cdee7e6db16
muhannadbm <mhd31145@hotmail.com>
2021-09-02 14:00:31 -0400

    react_express deployable

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config() //this allows to stash "artificial env variables in a
 const express = require('express')
 const cors = require('cors')
 const server = express()
+const path = require('path')
 
 
 const PORT = process.env.PORT || 4000
@@ -10,14 +11,18 @@ const PORT = process.env.PORT || 4000
 server.use(express.json())
 server.use(cors())
 
-server.get('/', (req,res)=> {
-    res.send('<h1>I am awesome </h1>')
-})
+//For deployment so heroku knows where static files are
+server.use(express.static(path.join(__dirname, 'client/build')))
+
+
 
 server.get('/api', (req,res)=>{
     res.json({message: 'i Rock'})
 })
 
+server.get('*', (req,res)=> {
+    res.sendFile(path.join(__dirname,'client/build', 'index.html'))
+})
 server.listen(PORT, ()=>{
     console.log('Listening on Port',PORT)
 })
